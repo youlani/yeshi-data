9d38a21f411a400c362cd79b6099c1ebb87c8c18
youlani <940135763@qq.com>
2021-02-21 19:37:30 +0800

    first

---
+++ src/test/java/com/example/yeshidata/YeshiDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yeshidata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeshiDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
